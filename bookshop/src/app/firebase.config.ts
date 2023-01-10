57bbb7a6dd20f16ec5478ecf47c4cfc8580e3bd7
Gaya3-developer <>
2023-01-10 10:43:25 +0530

    2 project added

---
+++ bookshop/src/app/firebase.config.ts
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAejjKPr6Ea6S0PmXOxCascxKk6PmMerr0",
+  authDomain: "ecommerceapp-e8343.firebaseapp.com",
+  projectId: "ecommerceapp-e8343",
+  storageBucket: "ecommerceapp-e8343.appspot.com",
+  messagingSenderId: "947108113528",
+  appId: "1:947108113528:web:e664e2b6b91c30bf2c918d"
+};
